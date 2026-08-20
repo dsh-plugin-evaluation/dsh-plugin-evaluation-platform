9c345876229fd214087994381586031b58f847c9
tree201 <40118061+tree201@users.noreply.github.com>
2026-08-20 22:49:57 +0800

    Export local HTTP API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { DshRuntimeError, resolveDshRuntime } from './runtime-config.js'
 export { DshHostBusyError, DshRunError, ManagedDshHost, redact } from './managed-dsh-host.js'
 export { JsonStorage, STORAGE_COLLECTIONS, StorageError } from './json-storage.mjs'
+export { createLocalApiServer } from './http-api.js'
